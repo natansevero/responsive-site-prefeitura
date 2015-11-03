f01d0c614cd6ce7de63443e46b244c14de43d41b
Natan <natanseverolp@gmail.com>
2015-11-03 21:24:47 -0200

    Modificando index.html e JS com evento de Click no Menu Mobile

---
+++ js/click.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('div.menu-oculto').click(function(){
+    $('#menu-mobile').fadeIn(500);
+  });
+
+  $('.fechar').click(function(event){
+		if (event.target != this) {
+			return;
+		}
+
+		$('#menu-mobile').fadeOut(500);
+	});
+
+
+});
